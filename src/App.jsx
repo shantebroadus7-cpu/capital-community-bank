2c35c44fc6df03e07674990c5bbce8ceb7cf9815
BOB G <shantebroadus7@gmail.com>
2026-05-24 07:36:56 -0700

    fix backend connection

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ export default function CapitalCommunityBank() {
       setLoading(true);
       setMessage("");
 
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch(
+  "https://capital-bank-api.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
