f640ea5e3a12d540862ebefb0b2168c9eddd7c9b
BOB G <shantebroadus7@gmail.com>
2026-05-25 20:45:39 -0700

    fixed professional auth system

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,44 +1,25 @@
 const mongoose = require("mongoose");
 
 const TransactionSchema =
-  new mongoose.Schema({
+  new mongoose.Schema(
 
-    sender: {
-      type: String,
-      required: true,
+    {
+      sender: String,
+      recipient: String,
+      bank: String,
+      accountNumber: String,
+      amount: Number,
+      status: {
+        type: String,
+        default: "Completed",
+      },
     },
 
-    recipient: {
-      type: String,
-      required: true,
-    },
+    {
+      timestamps: true,
+    }
 
-    bank: {
-      type: String,
-      required: true,
-    },
-
-    accountNumber: {
-      type: String,
-      required: true,
-    },
-
-    amount: {
-      type: Number,
-      required: true,
-    },
-
-    status: {
-      type: String,
-      default: "Completed",
-    },
-
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-
-  });
+  );
 
 module.exports =
   mongoose.model(
